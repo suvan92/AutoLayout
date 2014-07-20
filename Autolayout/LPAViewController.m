5a49d9c5d309f893b62fc0d6256e19b1186a677d
Steven Masuch <smasuch@electricmilk.com>
2014-07-20 11:34:25 -0700

    Add project files.

---
+++ Autolayout/LPAViewController.m
@@ -0,0 +1,29 @@
+//
+//  LPAViewController.m
+//  Autolayout
+//
+//  Created by Steven Masuch on 2014-07-20.
+//  Copyright (c) 2014 Lighthouse Labs. All rights reserved.
+//
+
+#import "LPAViewController.h"
+
+@interface LPAViewController ()
+
+@end
+
+@implementation LPAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
