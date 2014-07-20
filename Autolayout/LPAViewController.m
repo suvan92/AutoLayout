79813d39b349ff9b4a60b75e175da73c667b0ead
Steven Masuch <smasuch@electricmilk.com>
2014-07-20 12:12:11 -0700

    Set up beginning view and constraint code.

--- Autolayout/LPAViewController.m
+++ Autolayout/LPAViewController.m
@@ -10,6 +10,14 @@
 
 @interface LPAViewController ()
 
+@property (nonatomic, weak) UIButton *  squareButton;
+@property (nonatomic, weak) UIButton *  portraitButton;
+@property (nonatomic, weak) UIButton *  landscapeButton;
+
+@property (nonatomic, weak) UIView *                framingView;
+@property (nonatomic, weak) NSLayoutConstraint *    framingViewHeight;
+@property (nonatomic, weak) NSLayoutConstraint *    framingViewWidth;
+
 @end
 
 @implementation LPAViewController
@@ -17,13 +25,108 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *squareButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [squareButton setTitle:@"Square" forState:UIControlStateNormal];
+    [squareButton addTarget:self action:@selector(resizeFramingView:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:squareButton];
+    squareButton.translatesAutoresizingMaskIntoConstraints = NO;
+    self.squareButton = squareButton;
+    
+    UIButton *portraitButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [portraitButton setTitle:@"Portrait" forState:UIControlStateNormal];
+    [portraitButton addTarget:self action:@selector(resizeFramingView:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:portraitButton];
+    portraitButton.translatesAutoresizingMaskIntoConstraints = NO;
+    self.portraitButton = portraitButton;
+    
+    UIButton *landscapeButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [landscapeButton setTitle:@"Landscape" forState:UIControlStateNormal];
+    [landscapeButton addTarget:self action:@selector(resizeFramingView:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:landscapeButton];
+    landscapeButton.translatesAutoresizingMaskIntoConstraints = NO;
+    self.landscapeButton = landscapeButton;
+    
+    UIView *framingView = [[UIView alloc] initWithFrame:CGRectZero];
+    framingView.translatesAutoresizingMaskIntoConstraints = NO;
+    framingView.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:framingView];
+    self.framingView = framingView;
+    
+    NSString *buttonsHorizontalConstraints = @"|[squareButton(==portraitButton)][portraitButton(==landscapeButton)][landscapeButton]|";
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:buttonsHorizontalConstraints
+                                                                      options:NSLayoutFormatAlignAllCenterY
+                                                                      metrics:nil
+                                                                        views:NSDictionaryOfVariableBindings(squareButton, portraitButton, landscapeButton)]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:squareButton
+                                                          attribute:NSLayoutAttributeBottom
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeBottom
+                                                         multiplier:1.0
+                                                           constant:-50.0]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:framingView
+                                                          attribute:NSLayoutAttributeCenterX
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterX
+                                                         multiplier:1.0
+                                                           constant:0.0]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:framingView
+                                                          attribute:NSLayoutAttributeCenterY
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterY
+                                                         multiplier:1.0
+                                                           constant:-50.0]];
+    
+    NSLayoutConstraint *framingViewHeight = [NSLayoutConstraint constraintWithItem:framingView
+                                                                         attribute:NSLayoutAttributeHeight
+                                                                         relatedBy:NSLayoutRelationEqual
+                                                                            toItem:nil
+                                                                         attribute:NSLayoutAttributeNotAnAttribute
+                                                                        multiplier:1.0
+                                                                          constant:500.0];
+    
+    NSLayoutConstraint *framingViewWidth = [NSLayoutConstraint constraintWithItem:framingView
+                                                                        attribute:NSLayoutAttributeWidth
+                                                                        relatedBy:NSLayoutRelationEqual
+                                                                           toItem:nil
+                                                                        attribute:NSLayoutAttributeNotAnAttribute
+                                                                       multiplier:1.0
+                                                                         constant:500.0];
+    
+    [framingView addConstraint:framingViewHeight];
+    [framingView addConstraint:framingViewWidth];
+    
+    self.framingViewHeight = framingViewHeight;
+    self.framingViewWidth = framingViewWidth;
+    
+    // Set up your views and constraints here
+    
+    
 }
 
-- (void)didReceiveMemoryWarning
+- (void)resizeFramingView:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    CGFloat newWidth = 0.0;
+    CGFloat newHeight = 0.0;
+    
+    if (sender == self.squareButton) {
+        newWidth = 500.0;
+        newHeight = 500.0;
+    } else if (sender == self.portraitButton) {
+        newWidth = 350.0;
+        newHeight = 550.0;
+    } else if (sender == self.landscapeButton) {
+        newWidth = 900.0;
+        newHeight = 300.0;
+    }
+    
+    // Add in changing the constraints in an animated fashion here.
 }
 
 @end
